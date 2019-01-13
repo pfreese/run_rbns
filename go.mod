21ce08e1e88cf35ce3577c1172dfe307b3b719c8
Peter Freese <freese.peter@gmail.com>
2019-01-13 11:06:31 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pfreese/run_rbns
+
+require github.com/pfreese/rbns v0.0.0-20190113190019-7b34dfcf3aad
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/pfreese/rbns"
+)
+
+// The .json file containing settings
+const jsonF string = "../configs/gtex.json"
+
+func main() {
+
+	// Make a 'logs' directory in which to write log files
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	logDir := filepath.Join(pwd, "logs")
+	rbns.CreateDirIfNotExist(logDir)
+
+	// Set up the log file
+	// Mark the timestamp in the log file name: month_day_year_hour.minute.second
+	const layout= "01_02_2006_03.04.05"
+
+	tm := time.Now().Format(layout)
+	logBasename := fmt.Sprintf("log.%s.txt", tm)
+	logFile, err := os.OpenFile(filepath.Join(logDir, logBasename),
+		os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+	log.Print("Logging to a file in Go!")
+	// test case
+	log.Println("check to make sure it works")
+
+	// Parse the .json file
+	/*
+	configuration, nil := ParseJSON(jsonF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	*/
+}
